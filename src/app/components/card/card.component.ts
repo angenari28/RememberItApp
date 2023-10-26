3fc1054709534eab56382cae6d7ae02aaed5dcfd
Andre <andregenari28@gmail.com>
2023-10-26 18:49:19 -0300

    chore: adjust in style card and add clear text in card

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'ri-card',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.less'],
@@ -12,4 +13,6 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 export class CardComponent {
   @Input() removeCallback: Function = () => {};
   @Input() indexCard = 0;
+
+  protected textContent: string = '';
 }
